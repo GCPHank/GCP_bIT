c7e6cf9b9e5f653273e0e315c1f00686a1c4295d
Alexander Henkel <alexander.henkel@bridging-it.de>
2019-01-02 16:12:25 +0100

    Newest Version

--- Tutorial/calculator/calculator.js
+++ Tutorial/calculator/calculator.js
@@ -5,7 +5,7 @@ let j = 0;
 amount = prompt('Wieviel Apple Aktien möchtest du kaufen?', 10);
 
 if (amount <= 10) {
-    document.write(`Du kaufst ${amount} Aktien`);
+    document.write(`Du kaufst ${amount} Aktien!`);
 } else {
     alert('Das Datum darf nicht in der Zukunft liegen')
 }
